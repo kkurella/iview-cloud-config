3b824577101ef0a5035d64221a02ecbfdacef44f
kkurella <kreddy.re@gmail.com>
2025-01-30 15:28:34 -0500

    intial commit

---
+++ src/test/java/com/iview/iviewcloud/IviewCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iview.iviewcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IviewCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
